fd4754a1453d2ee672d41036bae2cf5af1df275b
bralexx <bruek.aiu@phystech.edu>
2021-02-23 19:12:33 +0300

    task1.3

--- task1/run.sh
+++ task1/run.sh
@@ -1,5 +1,9 @@
-folders=$(find $1 -type f -name "*.$2")
+paths=$(find $1 -type f -name "*.$2")
 mkdir $3
-cp $folders $3
-tar -czf $3/$4 $folders
+for path in $paths
+do
+	mkdir -p $3/$path
+	cp $path $3/$path
+done
+tar -czf $3/$4 $3/$1
 echo done
