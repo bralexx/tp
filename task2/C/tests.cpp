cb90de47ab62b0d666dc94c24969c35e935a3780
bralexx <bruek.aiu@phystech.edu>
2021-03-10 00:43:41 +0300

    v.0.4.1

--- task2/C/tests.cpp
+++ task2/C/tests.cpp
@@ -5,12 +5,12 @@
 TEST(IndexTest, test1) {
 	EXPECT_EQ(get(1), get(1));
 	EXPECT_EQ(get(0), get(0));
-	EXPECT_EQ(get(4), get(10));
+	EXPECT_EQ(get(10), get(10));
 }
 
 TEST(LibTest, test1) {
 	Class_test t;
 	EXPECT_EQ(t.f(1), t.f(1));
 	EXPECT_EQ(t.f(5), t.f(5));
-	EXPECT_EQ(t.f(2), t.f(3));
+	EXPECT_EQ(t.f(3), t.f(3));
 }
