a409660f909a4aa8ddff7301a1012664c994081c
bralexx <bruek.aiu@phystech.edu>
2021-04-18 21:46:16 +0300

    hgjdshkgsdfdsahoufdfgdshdf

--- task4/tests/03-weather/WeatherTestCase.cpp
+++ task4/tests/03-weather/WeatherTestCase.cpp
@@ -11,7 +11,7 @@ TEST(A, B) {
     	mock.SetApiKey("1");
 
     	ASSERT_EQ(mock.FindDiffBetweenTwoCities("1234 1234", "1234 1234"), 0);
-    	ASSERT_EQ(mock.FindDiffBetweenTwoCities("9000 1000", "1000 9000"), -8000);
+    	ASSERT_EQ(mock.FindDiffBetweenTwoCities("9000 1000", "1000 9000"), 8000);
     	ASSERT_EQ(mock.GetDifferenceString("1111 9999", "9999 1111"), "Weather in 1111 9999 is colder than in 9999 1111 by 8888 degrees");
     	ASSERT_EQ(mock.GetDifferenceString("8888 4444", "4444 8888"), "Weather in 8888 4444 is warmer than in 4444 8888 by 8888 degrees");
     	ASSERT_EQ(mock.GetTomorrowDiff("1000 1"), "The weather in 1000 1 tomorrow will be much colder than today.");
