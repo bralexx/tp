500f9efd24c75fdac0ff4a6f576af11adb1b2bbb
bralexx <bruek.aiu@phystech.edu>
2021-04-18 22:29:02 +0300

    hgjdshkgsdfdsahoufdfgdshdf

--- task4/src/02-tree/Tree.cpp
+++ task4/src/02-tree/Tree.cpp
@@ -10,28 +10,23 @@ using boost::filesystem::directory_iterator;
 FileNode GetTree(const std::string& path, bool dirs_only) {
   FileNode answer;
   if (!exists(path)) {
-	std::cout << 1 << '\n';
     throw std::invalid_argument("Path not exist");
   }
 
   if (!is_directory(path)) {
-	std::cout << 2 << '\n';
     throw std::invalid_argument("Path is not directory");
   }
   answer.name = boost::filesystem::path(path).filename().string();
   answer.is_dir = true;
   for (auto& child: directory_iterator(path)) {
     if (is_directory(child)){
-	std::cout << 3 << '\n';
       answer.children.push_back(
           GetTree(boost::filesystem::path(child).string(), dirs_only)
       );
     } else {
-	std::cout << 4 << '\n';
       if (dirs_only) {
         continue;
       }
-	std::cout << 5 << '\n';
       answer.children.push_back({
         boost::filesystem::path(child).filename().string(), false, {}
       });
@@ -43,17 +38,14 @@ FileNode GetTree(const std::string& path, bool dirs_only) {
 
 void FilterEmptyNodes(const FileNode& node, const path& current_path=".") {
     if (!node.is_dir) {
-	std::cout << 6  << '\n';
         return;
     }
 
     if (node.children.empty()) {
-std::cout << 7 << '\n';
         boost::filesystem::remove(current_path);
     }
     for (auto& child: node.children) {
-	std::cout << 8 << '\n';
-FilterEmptyNodes(child, current_path / child.name); 
+        FilterEmptyNodes(child, current_path / child.name); 
     }
 }
 
