54fc71cf26193e60a0baa04d45799e981487d441
bralexx <bruek.aiu@phystech.edu>
2021-04-18 20:38:44 +0300

    hgjdshkgsd

--- task4/tests/03-weather/WeatherTestCase.cpp
+++ task4/tests/03-weather/WeatherTestCase.cpp
@@ -15,17 +15,13 @@ TEST(A, B) {
     	} catch (...) {
     	}
 
-	for(int i = 0; i < 100; ++i) for(int j = 0; j < 100; ++j)
-    		ASSERT_EQ(a.GetDifferenceString(i + , "10 5"), "Weather in 5 10 is colder than in 10 5 by 5 degrees");
-    	ASSERT_EQ(a.GetDifferenceString("10 5", "5 10"), "Weather in 10 5 is warmer than in 5 10 by 5 degrees");
-    	ASSERT_EQ(a.FindDiffBetweenTwoCities("5 10", "10 5"), -5);
-    	ASSERT_EQ(a.GetTomorrowDiff("5 1"), "The weather in 5 1 tomorrow will be much colder than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 2"), "The weather in 5 2 tomorrow will be colder than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 3"), "The weather in 5 3 tomorrow will be colder than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 4"), "The weather in 5 4 tomorrow will be colder than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 5"), "The weather in 5 5 tomorrow will be the same than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 6"), "The weather in 5 6 tomorrow will be warmer than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 7"), "The weather in 5 7 tomorrow will be warmer than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 8"), "The weather in 5 8 tomorrow will be warmer than today.");
-    	ASSERT_EQ(a.GetTomorrowDiff("5 9"), "The weather in 5 9 tomorrow will be much warmer than today.");
+    	ASSERT_EQ(a.FindDiffBetweenTwoCities("1234 1234", "1234 1234"), 0);
+    	ASSERT_EQ(a.FindDiffBetweenTwoCities("9000 1000", "1000 9000"), -8000);
+    	ASSERT_EQ(a.GetDifferenceString("1111 9999", "9999 1111"), "Weather in 1111 9999 is colder than in 9999 1111 by 8888 degrees");
+    	ASSERT_EQ(a.GetDifferenceString("8888 4444", "4444 8888"), "Weather in 8888 4444 is warmer than in 4444 8888 by 8888 degrees");
+    	ASSERT_EQ(a.GetTomorrowDiff("1000 1"), "The weather in 1000 1 tomorrow will be much colder than today.");
+    	ASSERT_EQ(a.GetTomorrowDiff("1000 999"), "The weather in 1000 999 tomorrow will be colder than today.");
+    	ASSERT_EQ(a.GetTomorrowDiff("1000 1000"), "The weather in 1000 1000 tomorrow will be the same than today.");
+    	ASSERT_EQ(a.GetTomorrowDiff("1000 1001"), "The weather in 1000 1001 tomorrow will be warmer than today.");
+    	ASSERT_EQ(a.GetTomorrowDiff("1000 5000"), "The weather in 1000 5000 tomorrow will be much warmer than today.");
 }
